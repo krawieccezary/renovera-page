c1e796af724b7daa006717361071aec3a5c29a32
Cezary Krawiec <web@cezarykrawiec.pl>
2021-03-09 21:45:53 +0100

    Add Map to Page Home

--- src/components/organisms/RenoveraMap/RenoveraMap.jsx
+++ src/components/organisms/RenoveraMap/RenoveraMap.jsx
@@ -1,23 +1,59 @@
-import React, { Component } from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby';
+import { MapContainer, TileLayer, Marker, Popup, Circle, Icon } from 'react-leaflet';
+import Leaflet from 'leaflet';
+
+
+const circleCenter = [52.8687126, 18.0255176];
+const fillBlueOptions = { 
+  fillColor: 'hsl(49, 100%, 49%)', 
+  color: 'hsl(49, 100%, 49%)'
+};
 
 const RenoveraMap = () => {
-    if (typeof window !== 'undefined') {
-      return (
-        <MapContainer style={{height: '400px'}} center={[52.8687126, 18.0255176]} zoom={7} scrollWheelZoom={true}>
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[52.8687126, 18.0255176]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      )
+  const { file: {childImageSharp: {fixed: {src: image}}}} = useStaticQuery(graphql`
+      {
+      file(name: {regex: "/logo/i"}) {
+        childImageSharp {
+          fixed(height: 100, quality: 90){
+            src
+          }
+        }
+      }
     }
-    return null
+  `);
+  console.log(image);
+  // const MyIcon = Leaflet.Icon({
+  //   iconUrl: image,
+  //   iconSize: [38, 95],
+  //   iconAnchor: [22, 94],
+  //   popupAnchor: [-3, -76],
+  // });
+
+  
+
+  if (typeof window !== 'undefined') {
+    return (
+      <MapContainer 
+        style={{height: '400px'}} 
+        center={[52.8687126, 18.0255176]} 
+        zoom={7} 
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Circle center={circleCenter} pathOptions={fillBlueOptions} radius={100000} />
+        <Marker position={[52.8687126, 18.0255176]}>
+          <Popup>
+            Renovera
+          </Popup>
+        </Marker>
+      </MapContainer>
+    )
+  }
+  return null
 }
 
 export default RenoveraMap;
