e55d442dc6c1aab7a7d5ef7c0f37859f437dd87f
Cezary Krawiec <web@cezarykrawiec.pl>
2021-03-20 19:10:17 +0100

    Fetch offers data
    fetch offers data from DatoCMS and display it to offers page.

--- src/pages/oferta.jsx
+++ src/pages/oferta.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import { graphql, useStaticQuery } from 'gatsby';
+import Image from 'gatsby-image';
+import { SectionHeading, Paragraph } from '../components/index';
+
 
 const OfertaPage = () => {
+  const { allDatoCmsOffersPage: { nodes: contents }}  = useStaticQuery(graphql`
+    query offersPageQuery {
+      allDatoCmsOffersPage {
+        nodes {
+          offerTitle
+          offerDescription
+          id
+          offerImage {
+            fluid {
+              ...GatsbyDatoCmsFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <>
-      <h1>Oferta</h1>
+      <div className="wrapper">
+        {contents.map(content => (
+          <div>
+            <Image fluid={content.offerImage.fluid}/>
+            <SectionHeading>{content.offerTitle}</SectionHeading>
+            <Paragraph>{content.offerDescription}</Paragraph>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
