523388191aa1b383ec7382bde9615b0b991ecd34
Cezary Krawiec <web@cezarykrawiec.pl>
2021-04-03 16:05:40 +0200

    Styled grid layout

--- src/pages/oferta.jsx
+++ src/pages/oferta.jsx
@@ -6,9 +6,22 @@ import { SectionHeading, Paragraph } from '../components/index';
 
 const StyledWrapper = styled.div`
   display: grid;
-  grid-template-columns: 1.5fr 1fr;
+  grid-template-columns: 1.15fr .85fr;
   align-items: flex-start;
+  justify-content: flex-start;
+  grid-gap: 3rem;
+  margin: 5rem 0 7rem;
+  height: calc(100vh - 200px);
+
+  &:nth-child(2n) {
+    direction: rtl;
+  }
 `;
+
+const StyledContent = styled.div`
+  padding-top: 2rem;
+`;
+
 
 
 const OfertaPage = () => {
@@ -35,8 +48,10 @@ const OfertaPage = () => {
         {contents.map(content => (
           <StyledWrapper key={content.id}>
             <Image fluid={content.offerImage.fluid}/>
-            <SectionHeading special="true">{content.offerTitle}</SectionHeading>
-            <Paragraph>{content.offerDescription}</Paragraph>
+            <StyledContent>
+              <SectionHeading special="true">{content.offerTitle}</SectionHeading>
+              <Paragraph>{content.offerDescription}</Paragraph>
+            </StyledContent>
           </StyledWrapper>
         ))}
       </div>
