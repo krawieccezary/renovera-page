3a42c6b8bd90f36b11204189ec2728523bf04f9b
Cezary Krawiec <web@cezarykrawiec.pl>
2021-04-05 21:35:49 +0200

    Add Portfolio Context

--- src/components/molecules/PortfolioCategories/PortfolioCategories.jsx
+++ src/components/molecules/PortfolioCategories/PortfolioCategories.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import slugify from 'slugify';
 import styled from 'styled-components';
 import { graphql, useStaticQuery } from 'gatsby';
+
 import { Button } from '../../index';
+import PortfolioContext from '../../../context/PortfolioContext';
 
 const StyledWrapper = styled.div`
   display: flex;
@@ -28,8 +30,9 @@ const PortfolioCategories = () => {
       }
     }
   `);
-
   const categories = getUniqueCategories(items);
+
+  const activeCategory = useContext(PortfolioContext);
 
   return (
     <>
@@ -37,7 +40,7 @@ const PortfolioCategories = () => {
         {categories.map(category => (
           <Button
             as="button"
-            color="primary" 
+            color={slugify(category) === activeCategory ? 'black' : 'primary'} 
             data-category={slugify(category)} 
             key={category}>
             {category}
